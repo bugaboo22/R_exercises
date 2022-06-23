e43999188b0629b54ffc545561039eb16a06c14d
Lauri Vastela <lauri.vastela@gmail.com>
2022-06-23 13:57:16 +0300

    commit

--- DatasauRus.R
+++ DatasauRus.R
@@ -4,3 +4,14 @@ data("quartet")
 View(quartet)
 
 install.packages("tidyverse")
+library(tidyverse)
+
+install.packages("ggplot2")
+library(ggplot2)
+
+install.packages("datasauRus")
+library(datasauRus)
+
+ggplot(datasaurus_dozen,aes(x=x, y=y, colour=dataset)) +
+  geom_point() +theme(legend.position = "none")+facet_wrap(~dataset)
+
